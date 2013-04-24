0b0dea7ed56b2eef13b30bf88bd9ec9086104043
Claus Klingberg <cjk@pobox.com>
2013-04-24 14:21:26 +0200

    Add my own customizations so emacs.js fits into cjk's existing emacs-setup.

--- config/basic-bindings.el
+++ config/basic-bindings.el
@@ -2,9 +2,9 @@
 (global-set-key (kbd "M-g u") 'undo)
 (global-set-key (kbd "M-g r") 'redo)
 
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
+;; (global-set-key "\C-w" 'backward-kill-word)
+;; (global-set-key "\C-x\C-k" 'kill-region)
+;; (global-set-key "\C-c\C-k" 'kill-region)
 
 ;;(global-set-key [up] nil)
 ;;(global-set-key [down] nil)
